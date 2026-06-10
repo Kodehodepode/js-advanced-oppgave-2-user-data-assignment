b47910dbd99ade44f5b849d1542448fcd11472b2
Kodehodepode <kare.kodehode@gmail.com>
2026-06-10 15:33:34 +0200

    Element references

---
+++ script.js
@@ -0,0 +1,6 @@
+const fridgeTable = document.getElementById("fridge-table");
+const fridgeBody = document.getElementById("fridge-body");
+const fridgeFooter = document.getElementById("fridge-footer");
+
+const sumProducts = document.getElementById("sum-products");
+const sumExpired = document.getElementById("sum-expired");
